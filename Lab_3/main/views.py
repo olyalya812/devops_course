75ba2f85cf745d9816046406f25b8a9ae9b8a1e6
olyalya <olha.valchak.itis.2018.lpnu.ua>
2020-11-29 23:42:39 +0200

    Added everything before 10 zavdannya

--- Lab_3/main/views.py
+++ Lab_3/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    response = {'date': 'test1', 'current_page': "test2", 'server_info': "test3", 'client_info': "test4"}
+    return JsonResponse(response)
